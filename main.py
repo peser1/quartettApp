cf53aee2ab278b096d5369d9bbd751fd839292b8
peser1 <p.eser1@freenet.de>
2017-03-29 00:11:24 +0200

    set image to fullscreen

--- main.py
+++ main.py
@@ -11,47 +11,48 @@ class ProgramX(App):
 
 		self.f = FloatLayout()
 
-		self.bPower = Button(text='', font_size=20, background_color=[0, 0, 0, 0], \
+		self.b1 = Button(text='', font_size=20, background_color=[0, 0, 0, 0], \
 				pos_hint={'center_x': 0.5, 'center_y': 0.485}, size_hint=(0.75,0.08))
-		self.bCO2 = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
+		self.b2 = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
 				pos_hint={'center_x': 0.5, 'center_y': 0.405}, size_hint=(0.75,0.08))
-		self.bCost = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
+		self.b3 = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
 				pos_hint={'center_x': 0.5, 'center_y': 0.325}, size_hint=(0.75,0.08))
-		self.bEmployee = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
+		self.b4 = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
 				pos_hint={'center_x': 0.5, 'center_y': 0.245}, size_hint=(0.75,0.08))
-		self.bFootprint = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
+		self.b5 = Button(text='', font_size=20,  background_color=[0, 0, 0, 0], \
 				pos_hint={'center_x': 0.5, 'center_y': 0.165}, size_hint=(0.75,0.08))
 
-		self.iCard = Image(source='Hydro1.jpg', pos_hint={'center_x': 0.5, 'center_y': 0.5}, size_hint=(1.0,1.0))
+		self.iCard = Image(source='imgs/Hydro1_small.jpg', allow_stretch=True, keep_ratio=False, \
+				pos_hint={'center_x': 0.5, 'center_y': 0.5}, size_hint=(1.0,1.0))
 		self.labl = Label(text='', font_size=65, color=[1, 1, 1, 1], \
 				pos_hint={'center_x': 0.5, 'center_y': 0.85}, size_hint=(None,None))
 
-		self.bPower.bind(on_press=self.selectOption)
-		self.bCO2.bind(on_press=self.selectOption)
-		self.bCost.bind(on_press=self.selectOption)
-		self.bEmployee.bind(on_press=self.selectOption)
-		self.bFootprint.bind(on_press=self.selectOption)
+		self.b1.bind(on_press=self.selectOption)
+		self.b2.bind(on_press=self.selectOption)
+		self.b3.bind(on_press=self.selectOption)
+		self.b4.bind(on_press=self.selectOption)
+		self.b5.bind(on_press=self.selectOption)
 
 		self.f.add_widget(self.iCard)
-		self.f.add_widget(self.bPower)
-		self.f.add_widget(self.bCO2)
-		self.f.add_widget(self.bCost)
-		self.f.add_widget(self.bEmployee)
-		self.f.add_widget(self.bFootprint)
+		self.f.add_widget(self.b1)
+		self.f.add_widget(self.b2)
+		self.f.add_widget(self.b3)
+		self.f.add_widget(self.b4)
+		self.f.add_widget(self.b5)
 		self.f.add_widget(self.labl)
 
 		return self.f
 
 	def selectOption(self, instance):
-		if instance == self.bPower:
+		if instance == self.b1:
 			self.labl.text = 'Pressed Power!'
-		elif instance == self.bCO2:
+		elif instance == self.b2:
 			self.labl.text = 'Pressed CO2!'
-		elif instance == self.bCost:
+		elif instance == self.b3:
 			self.labl.text = 'Pressed Cost!'
-		elif instance == self.bEmployee:
+		elif instance == self.b4:
 			self.labl.text = 'Pressed Employees!'
-		elif instance == self.bFootprint:
+		elif instance == self.b5:
 			self.labl.text = 'Pressed Footprint!'
 		return 0
 
